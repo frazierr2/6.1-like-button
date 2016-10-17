42b94394a4aac3c9c9d9102f74dad5385b3f38aa
Ryan Frazier <frazierr2@gmail.com>
2016-10-17 16:01:37 -0400

    button finished part 1

---
+++ app/scripts/index.js
@@ -0,0 +1,16 @@
+var Backbone = require('backbone');
+var $ = require('jquery');
+var ButtonClick = require('./models/like').ButtonClick;
+
+var click = new ButtonClick();
+var startNum = click.get('clickCount');
+// console.log(startNum);
+// console.log('click', click)
+
+//
+$('#like-button').on("click", function() {
+  click.like();
+  // console.log(click.get('clickCount'));
+  $('.like-num').text(click.get('clickCount'));
+
+});
--- app/scripts/models/like.js
+++ app/scripts/models/like.js
@@ -1,1 +1,14 @@
-dsdfdf
+var Backbone = require('backbone');
+
+var ButtonClick = Backbone.Model.extend({
+  defaults: {
+    clickCount: 0
+  },
+  like: function() {
+    this.set('clickCount', this.get('clickCount') + 1);
+  }
+});
+
+module.exports = {
+  "ButtonClick": ButtonClick
+};
